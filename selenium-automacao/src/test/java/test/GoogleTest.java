9e48a2bddd195688235e76fcd75b74c4a231131a
Tiago Mota <tmota.seg@gmail.com>
2026-07-24 16:38:29 -0300

    Feat: Criação da variável 'caminhoDriver'.

--- selenium-automacao/src/test/java/test/GoogleTest.java
+++ selenium-automacao/src/test/java/test/GoogleTest.java
@@ -4,5 +4,6 @@ import org.openqa.selenium.WebDriver;
 
 public class GoogleTest {
     private WebDriver driver;
-    private final String URL_BASE = "https://www.google.com/";
+    private final String urlBase = "https://www.google.com/";
+    private final String caminhoDriver = "/home/atom/Documents/QA/JAVA/JAVA-Selenium/SELENIUM/selenium-automacao/src/test/java/resource/chromedriver-v150";
 }
