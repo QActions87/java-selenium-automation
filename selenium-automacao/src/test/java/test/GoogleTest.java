f2acbdc7c1dd81e78a82613b2d8af3e968d21820
Tiago Mota <tmota.seg@gmail.com>
2026-07-24 16:52:24 -0300

    Feat: Importando o ChromeDriver e criando as funções 'iniciar()' e 'devePesquisarNoGoogle()'.

--- selenium-automacao/src/test/java/test/GoogleTest.java
+++ selenium-automacao/src/test/java/test/GoogleTest.java
@@ -1,9 +1,21 @@
 package test;
 
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleTest {
     private WebDriver driver;
     private final String urlBase = "https://www.google.com/";
     private final String caminhoDriver = "/home/atom/Documents/QA/JAVA/JAVA-Selenium/SELENIUM/selenium-automacao/src/test/java/resource/chromedriver-v150";
+
+    private void iniciar() {
+        System.setProperties("webdriver.chrome.driver", caminhoDriver);
+    }
+
+    @Test
+    public void devePesquisarNoGoogle() {
+        iniciar();
+    }
+
 }
