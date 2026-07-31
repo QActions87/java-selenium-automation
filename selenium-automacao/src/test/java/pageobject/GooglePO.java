659da7a334105a7e5e3dffcdc99d1cfef480871f
Tiago Mota <tmota.seg@gmail.com>
2026-07-31 01:15:10 -0300

    Feat: Mapeia a caixa de texto de busca do Google através do atributo 'name=q' do HTML Com Page Factory.

--- selenium-automacao/src/test/java/pageobject/GooglePO.java
+++ selenium-automacao/src/test/java/pageobject/GooglePO.java
@@ -1,8 +1,13 @@
 package pageobject;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class GooglePO extends BasePO {
+
+    @FindBy(name = "q")
+    public WebElement inputPesquisa;
 
     /**
      * Construtor para criação da página do Google:
