7eee0ad3d0a1449d7705b99fbf5078bbe9fdf2d5
Tiago Mota <tmota.seg@gmail.com>
2026-07-31 01:02:12 -0300

    Feat: Criado a 1ª Page Objetc 'GooglePO', 'extends BasePO', com o construtor da classe pai.

---
+++ selenium-automacao/src/test/java/pageobject/GooglePO.java
@@ -0,0 +1,16 @@
+package pageobject;
+
+import org.openqa.selenium.WebDriver;
+
+public class GooglePO extends BasePO {
+
+    /**
+     * Construtor com parâmetro:
+     * Construtor base para criação da fábrica de elementos:
+     *
+     * @param driver driver da página atual.
+     */
+    protected GooglePO(WebDriver driver) {
+        super(driver);
+    }
+}
