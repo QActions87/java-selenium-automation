4854cb8dd3b098be9eacdb29e4d7b3a8b354db05
Tiago Mota <tmota.seg@gmail.com>
2026-08-03 17:44:41 -0300

    Feat: Chamando o método 'inputPesquisa.sendKeys()' na instância 'googlePage'.

--- selenium-automacao/src/test/java/test/GoogleTeste.java
+++ selenium-automacao/src/test/java/test/GoogleTeste.java
@@ -4,7 +4,6 @@ import org.junit.BeforeClass;
 import org.junit.Test; // Importa a anotação @Test do JUnit para indicar que o method é um teste automatizado
 import org.openqa.selenium.By; // Importa a classe By, usada para definir estratégias de localização de elementos (id, name, className, etc.)
 import org.openqa.selenium.Keys; // Importa a enumeração Keys, usada para simular o pressionamento de teclas do teclado (ex: ENTER, TAB, BACK_SPACE)
-import org.openqa.selenium.WebElement; // Importa a interface que representa um elemento HTML manipulável na página web
 import org.openqa.selenium.support.ui.ExpectedConditions; // Importa a classe de condições esperadas do Selenium, usada para verificar estados de elementos (visibilidade, clique, etc.)
 import org.openqa.selenium.support.ui.WebDriverWait; // Importa a classe de espera explícita do Selenium, responsável por pausar a execução até uma condição ser atendida
 import pageobject.GooglePO;
@@ -27,10 +26,10 @@ public class GoogleTeste extends BaseTest {
     public void devePesquisarNoGoogle() {
         // Mapeia a caixa de texto de busca do Google através do atributo 'name="q"' do HTML
 
-        WebElement inputPesquisa = driver.findElement(By.name("q"));
+        //WebElement inputPesquisa = driver.findElement(By.name("q"));
 
         // Digita a palavra "café" no campo de busca e emula o pressionamento da tecla ENTER para submeter a pesquisa
-        inputPesquisa.sendKeys("café" + Keys.ENTER);
+        googlePage.inputPesquisa.sendKeys("café" + Keys.ENTER);
 
         // Cria a instância de espera explícita configurada para aguardar até 10 segundos no Selenium 3
         WebDriverWait wait = new WebDriverWait(driver, 10);
