3438dd1c0503ba6f266df98da52fef242a278036
Tiago Mota <tmota.seg@gmail.com>
2026-08-04 15:43:13 -0300

    Feat: 2º Projeto: Criação da classe LoginPO extends BasePO e invocação do construtor da super classe(BasePO - classe pai).

---
+++ selenium-automacao/src/test/java/pageobject/LoginPO.java
@@ -0,0 +1,14 @@
+package pageobject;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPO extends BasePO {
+    /**
+     * Construtor com parâmetro:
+     * Construtor base para criação da fábrica de elementos:
+     * @param driver driver da página atual.
+     */
+    protected LoginPO(WebDriver driver) {
+        super(driver);
+    }
+}
