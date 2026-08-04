538f06ea7ff07ee055acd31b3f6e55cf90e57f0a
Tiago Mota <tmota.seg@gmail.com>
2026-08-04 16:29:15 -0300

    Feat: 2º Projeto: Mapeando o campo email com PageFactory.

--- selenium-automacao/src/test/java/pageobject/LoginPO.java
+++ selenium-automacao/src/test/java/pageobject/LoginPO.java
@@ -1,11 +1,17 @@
 package pageobject;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPO extends BasePO {
+    // Mapeia a caixa de texto de busca do Google através do atributo 'name="q"' do HTML Com Page Factory:
+    @FindBy(id = "email")
+    public WebElement inputEmail;
+
     /**
      * Construtor com parâmetro:
-     * Construtor base para criação da fábrica de elementos:
+     * invocação do construtor canônico para criação da instância filha::
      * @param driver driver da página atual.
      */
     protected LoginPO(WebDriver driver) {
