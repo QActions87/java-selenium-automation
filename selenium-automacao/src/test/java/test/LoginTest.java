2af62c838440145c57ee834c1fdf43ff72ce3028
Tiago Mota <tmota.seg@gmail.com>
2026-08-05 20:43:13 -0300

    Feat: Criando 2º caso de teste 'TC002_naoDeveLogarNoSistemaComEmailIncorretoESenhaVazia()', reaproveitanda estrutura do 1º.

--- selenium-automacao/src/test/java/test/LoginTest.java
+++ selenium-automacao/src/test/java/test/LoginTest.java
@@ -15,10 +15,22 @@ public class LoginTest extends BaseTest {
         loginPage = new LoginPO(driver);
     }
 
+    // 1º Caso de Teste:
     // Anotação do JUnit que identifica este method como um caso de teste executável
     @Test
     public void TC001_naoDeveLogarNoSistemaComEmailESenhaVazios() {
         loginPage.escrever(loginPage.inputEmail, "");
+        loginPage.escrever(loginPage.inputSenha, "");
+        loginPage.btnEntrar.click();
+        String mensagem = loginPage.obterMensagem();
+        assertEquals("Informe usuário e senha, os campos não podem ser brancos.", mensagem);
+
+    }
+
+    // 2º Caso de Teste:
+    @Test
+    public void TC002_naoDeveLogarNoSistemaComEmailIncorretoESenhaVazia() {
+        loginPage.escrever(loginPage.inputEmail, "teste");
         loginPage.escrever(loginPage.inputSenha, "");
         loginPage.btnEntrar.click();
         String mensagem = loginPage.obterMensagem();
