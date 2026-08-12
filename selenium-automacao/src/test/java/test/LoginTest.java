1c0f30f9a9619850f3514d7e427a9bcbf1ef944c
Tiago Mota <tmota.seg@gmail.com>
2026-08-11 21:25:16 -0300

    Fix: Alterando a frase esperada dos testes com ambos os campos não vazios.

--- selenium-automacao/src/test/java/test/LoginTest.java
+++ selenium-automacao/src/test/java/test/LoginTest.java
@@ -1,11 +1,14 @@
 package test;
 
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import pageobject.LoginPO;
 
 import static org.junit.Assert.assertEquals;
-
+// Orientação do JUnit para que os testes sejam executados em ordem numérica crescente:
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class LoginTest extends BaseTest {
     // Atributo:
     private static LoginPO loginPage;
@@ -46,7 +49,7 @@ public class LoginTest extends BaseTest {
     public void TC004_naoDeveLogarNoSistemaComEmailIncorretoESenhaIncorreta() {
         loginPage.executarAcaoDeLogar("teste", "teste");
         String mensagem = loginPage.obterMensagem();
-        assertEquals("Informe usuário e senha, os campos não podem ser brancos.", mensagem);
+        assertEquals("E-mail ou senha inválidos", mensagem);
     }
 
     // 5º Caso de Teste:
@@ -54,7 +57,7 @@ public class LoginTest extends BaseTest {
     public void TC005_naoDeveLogarNoSistemaComEmailCorretoESenhaIncorreta() {
         loginPage.executarAcaoDeLogar("admin@admin.com", "teste");
         String mensagem = loginPage.obterMensagem();
-        assertEquals("Informe usuário e senha, os campos não podem ser brancos.", mensagem);
+        assertEquals("E-mail ou senha inválidos", mensagem);
     }
 
     // 6º Caso de Teste:
@@ -62,6 +65,6 @@ public class LoginTest extends BaseTest {
     public void TC006_naoDeveLogarNoSistemaComEmailIncorretoESenhaCorreta() {
         loginPage.executarAcaoDeLogar("Aadmin@admin.com", "admin@123");
         String mensagem = loginPage.obterMensagem();
-        assertEquals("Informe usuário e senha, os campos não podem ser brancos.", mensagem);
+        assertEquals("E-mail ou senha inválidos", mensagem);
     }
 }
