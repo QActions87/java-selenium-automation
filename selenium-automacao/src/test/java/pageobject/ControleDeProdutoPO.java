545220878f812154948ac4cca5dddbc6b8aa81cd
Tiago Mota <tmota.seg@gmail.com>
2026-08-12 23:36:41 -0300

    Feat: Iniciando classe de Page Object 'ControleDeProdutoPO' da página Controle de produtos.

---
+++ selenium-automacao/src/test/java/pageobject/ControleDeProdutoPO.java
@@ -0,0 +1,45 @@
+package pageobject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ControleDeProdutoPO extends BasePO{
+
+    @FindBy(id = "btn-adicionar")
+    private WebElement
+    /**
+     * Construtor com parâmetro:
+     * Construtor base para criação da fábrica de elementos:
+     *
+     * @param driver driver da página atual.
+     */
+    public ControleDeProdutoPO(WebDriver driver) {
+        super(driver);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
