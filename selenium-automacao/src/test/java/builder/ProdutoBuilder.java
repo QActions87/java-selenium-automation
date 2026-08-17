845c74d8debf5e1088a823335e5bf392f1ee4de3
Tiago Mota <tmota.seg@gmail.com>
2026-08-17 17:26:52 -0300

    Feat: Padrão de Projeto Builder: É um padrão criacional que permite construir objetos complexos passo a passo por meio de chamadas encadeadas de métodos (interface fluente). . Criando os atributos da classe ProdutoBuilder.

---
+++ selenium-automacao/src/test/java/builder/ProdutoBuilder.java
@@ -0,0 +1,9 @@
+package builder;
+
+public class ProdutoBuilder {
+    public String codigo;
+    public String nome;
+    public Integer quantidade;
+    public Double valor;
+    public String data;
+}
