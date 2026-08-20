4cac4f83e16b7a27bd60b15f841f001c40e805d4
Tiago Mota <tmota.seg@gmail.com>
2026-08-20 14:03:24 -0300

    Feat: Os atributos 'valor' e 'quantidade' da classe ProdutoBuilder estavam definidos como 'String' para facilitar o teste. No entanto, me dei conta de que vou precisar implementar a solução para resolver isso, quando precisar, por exemplo, usar a técnica de valor limite. Pois, com ela como string não seria eficiente. Então, desta forma eu treino.

--- selenium-automacao/src/test/java/builder/ProdutoBuilder.java
+++ selenium-automacao/src/test/java/builder/ProdutoBuilder.java
@@ -6,8 +6,8 @@ public class ProdutoBuilder {
     // Valores padrão:
     public String codigo = "00001";
     public String nome = "Produto Padrão";
-    public String quantidade = "1";
-    public String valor = "1.0";
+    public Integer quantidade = 1;
+    public Double valor = 1.0;
     public String data = "17/08/2026";
     private ControleDeProdutoPO controleDeProdutoPO;
 
