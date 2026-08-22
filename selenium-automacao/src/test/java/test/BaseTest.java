43fd20850ef3a72cec39159510da3f1d5584c00c
Tiago Mota <tmota.seg@gmail.com>
2026-08-21 23:34:18 -0300

    Feat: Documentanto a classe 'BaseTest.'

--- selenium-automacao/src/test/java/test/BaseTest.java
+++ selenium-automacao/src/test/java/test/BaseTest.java
@@ -5,9 +5,10 @@ import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.util.Collections;
 
+/**
+ *  Classe base que serve de herança para todas as classes de testes */
 @SuppressWarnings("ALL")
 public abstract class BaseTest {
     // Atributo privado para armazenar e reutilizar a instância do navegador nos testes
